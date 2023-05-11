04b83fc5e65aba10d0f0d65994b790ab7b98edf6
d33pster <deep.main.ac@gmail.com>
2023-05-11 08:51:01 +0530

    v2.1
    ~Performance upgrade

--- webrep.init.sh
+++ webrep.init.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 echo "Checking ... "
 sleep 2
+
 Dir='./output-files/'
-if [ -d "$Dir" ]; then
-  sudo apt install python3-pip -y
-  python3 -m pip install --upgrade pip
-  pip3 install requests
-else
+
+if [ ! -d "$Dir" ]; then
   echo " "
   echo "Initializing first time setup ..."
   sleep 1s
   echo "Initializing output directory ..."
   sleep 1s
   mkdir output-files
-  sudo apt install python3-pip -y
-  python3 -m pip install --upgrade pip
-  pip3 install requests
 fi
+
+sudo apt install python3-pip -y
+python3 -m pip install --upgrade pip
+pip3 install requests
